b31a41d362adfb7c557f286bd977492563d838c3
lucasbelusso1 <belussolucas@gmail.com>
2020-08-14 11:20:48 -0300

    Adiciona mobile

---
+++ aulas/mobile/src/pages/Landing/index.tsx
@@ -0,0 +1,77 @@
+import React from 'react';
+import { View, Image, Text } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { RectButton } from 'react-native-gesture-handler';
+
+import styles from './styles';
+
+import landingImg from '../../assets/images/landing.png';
+import studyIcon from '../../assets/images/icons/study.png';
+import giveClassesIcon from '../../assets/images/icons/give-classes.png';
+import heartIcon from '../../assets/images/icons/heart.png';
+import { AppLoading } from 'expo';
+
+import { Archivo_400Regular, Archivo_700Bold, useFonts } from '@expo-google-fonts/archivo';
+import { Poppins_400Regular, Poppins_600SemiBold } from '@expo-google-fonts/poppins';
+
+function Landing() {
+  const { navigate } = useNavigation();
+
+  function handleNavigateToGiveClassesPage() {
+    navigate('GiveClasses');
+  }
+
+  function handleNavigateToStudy() {
+    navigate('Study');
+  }
+
+  let [fontsLoaded] = useFonts({
+    Archivo_400Regular,
+    Archivo_700Bold,
+    Poppins_400Regular,
+    Poppins_600SemiBold
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  } else {
+
+    return (
+      <View style={styles.container}>
+
+        <Image source={landingImg} style={styles.banner} />
+        <Text style={styles.title}>
+          Seja bem vindo, {'\n'}
+          <Text style={styles.titleBold}> O que deseja fazer?</Text>
+        </Text>
+
+        <View style={styles.buttonsContainer}>
+
+          <RectButton 
+            style={[styles.button, styles.buttonPrimary]}
+            onPress={handleNavigateToStudy}
+          >
+             <Image source={studyIcon}/>
+             <Text style={styles.buttonText}>Estudar</Text>
+          </RectButton>
+
+          <RectButton 
+            onPress={handleNavigateToGiveClassesPage} 
+            style={[styles.button, styles.buttonSecondary]}
+          >
+             <Image source={giveClassesIcon}/>
+             <Text style={styles.buttonText}>Dar aulas</Text>
+          </RectButton>
+
+        </View>
+
+        <Text style={styles.totalConnections}>
+          Total de 285 conexões já realizadas {' '}
+          <Image source={heartIcon} />
+        </Text>
+      </View>
+    );
+  }
+}
+
+export default Landing;
